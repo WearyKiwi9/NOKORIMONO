77c07469bebcfc8142282bb0185b9cf61581ebad
Tejal Patel <tpatel@tejals-macbook-pro.local>
2019-02-26 15:29:21 -0800

    Implemented profile and banner image camera launch. Has bugs. To fix: adjust scaling of image for profile/banner; save image for profile/banner; allow user to choose whether to take a picture or choose from photo library

--- Nokorimono/ProfileViewController.swift
+++ Nokorimono/ProfileViewController.swift
@@ -7,10 +7,67 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var bannerImage: UIImageView!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    lazy var imageObject = {self.profileImage}()
+    var imageName = "profile"
+    //var imageObject = self.profileImage
+    
+    @IBOutlet var onProfileButton: UITapGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func onProfileButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+        imageObject = self.profileImage
+        imageName = "profile"
+    }
+    
+    @IBAction func onBannerButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+        imageObject = self.bannerImage
+        imageName = "banner"
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        var size = CGSize(width: 375, height: 140)
+        // THERE IS A PROBLEM WITH SIZING
+        if imageName == "profile" {
+            var size = CGSize(width: 140, height: 140)
+        }
+        
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageObject?.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
     }
 }
